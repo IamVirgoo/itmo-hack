e9e36725119a060610b20ef64f08572ae937b004
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-04-14 20:50:13 +0300

    WIP: hero section separator

--- app/src/pages/indexPage.tsx
+++ app/src/pages/indexPage.tsx
@@ -1,5 +1,9 @@
+import { Link } from "react-router-dom";
+
 import Card from "../components/card";
 import Header from "../components/header";
+
+import arrow from '../assets/arrow.svg'
 
 export default function IndexPage() {
     return <main>
@@ -8,6 +12,14 @@ export default function IndexPage() {
             <div className={'hero--container'}>
                 <h1 className={'hero--container--title'}>Рекомендуемые темы</h1>
                 <Card type={'main'}/>
+                <div className={'hero--container--separator'}>
+                    <div className={'line'}></div>
+                    <a href={'#'} className={'hero--container--separator--button'}>
+                        <p className={'hero--container--separator--button--link'}>Показать ещё</p>
+                        <img src={arrow} alt=""/>
+                    </a>
+                    <div className={'line'}></div>
+                </div>
             </div>
         </section>
         <section className={'selected'}>
